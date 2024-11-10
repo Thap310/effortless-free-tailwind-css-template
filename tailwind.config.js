1acedce8e57d5b7636f146415965f69fa740cd2a
Thapelo Mohlala <temohlala@gmail.com>
2024-11-10 21:57:13 +0200

    uploading to server

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,34 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./**/*.{html,js}"],
+
+const withMT = require("@material-tailwind/html/utils/withMT");
+
+module.exports = withMT({
+  content: ["./**/*.{html,js}", "node_modules/preline/dist/*.js"],
   theme: {
     container: {
       center: true,
-      padding: '1rem',
+      padding: "1rem",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      black: "#08111F",
+      primary: "#265380",
+      blueCustom: "#265380",
+      secondary: "#265380",
+    },
+    extend: {
+      fontFamily: {
+        roboto: ["roboto"],
+      },
+    },
   },
-  screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-  },
-  colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      white: '#ffffff',
-      black: '#08111F',
-      primary: '#4F46E5',
-      secondary: '#ea47ed', 
-  },
-    extend: {},
-  },
-  plugins: [],
-}
-
+  plugins: [require("daisyui"), require("preline/plugin")],
+});
